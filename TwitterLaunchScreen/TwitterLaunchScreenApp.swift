a77d334a8777fd5595db4a08da1d9820cbf580ad
JGodwyn <60453471+JGodwyn@users.noreply.github.com>
2024-11-12 20:36:42 +0100

    Initial Commit

---
+++ TwitterLaunchScreen/TwitterLaunchScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterLaunchScreenApp.swift
+//  TwitterLaunchScreen
+//
+//  Created by Gdwn on 12/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct TwitterLaunchScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
