f18115e811653925f0703a7cade933d160e1de52
JGodwyn <60453471+JGodwyn@users.noreply.github.com>
2024-12-06 21:01:06 +0100

    testing the push status of git

---
+++ TwitterLaunchScreen/LogIn.swift
@@ -0,0 +1,18 @@
+//
+//  LogIn.swift
+//  TwitterLaunchScreen
+//
+//  Created by Gdwn on 06/12/2024.
+//
+
+import SwiftUI
+
+struct LogIn: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    LogIn()
+}
